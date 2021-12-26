9c83725ea504844aee1ff2aa121da387cb485d3b
tarkerin <feelthegamesense@gmail.com>
2021-12-26 10:37:38 -0300

    kkk

--- tScan.py
+++ tScan.py
@@ -1,18 +1,20 @@
 import socket
 
-site = input('Digite o site ou o Ip alvo: ')
-qtd = (int(input('Quantidade de portas: ')))
+site = input('Write the Url or Ip Address: ')
+qtd = (int(input('Quantity of ports you want test: ')))
 portas = []
-
+ip = (socket.gethostbyname(site))
+dominio = (socket.gethostbyaddr(ip))
+dominio = dominio.split()[0] ## arrumar isso
 for x in range(qtd):
-    portas.append(int(input('Porta: ')))
+    portas.append(int(input('Port: ')))
 for porta in portas:
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.settimeout(0.5)
     codigo = cliente.connect_ex((site, porta))
     if codigo == 0:
-        print('\033[92m','[*]','\033[0m',porta, 'OPEN') ## arrumar isso
+        print('\033[92m','[*]','\033[0m',ip, dominio,porta, 'OPEN') 
     else:
-        print('\033[91m','[*]','\033[0m',porta, 'CLOSED') ## arrumar isso
+        print('\033[91m','[*]','\033[0m',ip, dominio,porta, 'CLOSED') 
 
 input()
